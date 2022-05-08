4a1c6bbe09372b65c3a4c107990106f67304f13a
cliche <745356675@qq.com>
2022-05-08 20:31:09 +0800

    -

--- threads/scheduler.cc
+++ threads/scheduler.cc
@@ -111,9 +111,9 @@ Scheduler::Run (Thread *nextThread)
     
 #ifdef USER_PROGRAM			// ignore until running user programs 
     if (currentThread->pcb->space != NULL) {	// if this thread is a user program,
-        DEBUG('t', "Save user program state.\n");
         currentThread->pcb->SaveUserState(); // save the user's CPU registers
 	    currentThread->pcb->space->SaveState();
+        DEBUG('t', "Save user program state.\n");
     }
 #endif
     
@@ -154,6 +154,8 @@ Scheduler::Run (Thread *nextThread)
     if (currentThread->pcb->space != NULL) {		// if there is an address space
         currentThread->pcb->RestoreUserState();     // to restore, do it.
 	    currentThread->pcb->space->RestoreState();
+        printf(currentThread->pcb->space);
+        currentThread->pcb->space->Print();
         DEBUG('t', "Restore user program state.\n");
     }
 #endif
