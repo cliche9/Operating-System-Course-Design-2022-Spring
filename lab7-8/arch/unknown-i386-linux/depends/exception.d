55496e8800d3ddf573571f77f66d7c79198c2a6b
cliche <745356675@qq.com>
2022-05-10 01:08:58 +0800

    -

--- lab7-8/arch/unknown-i386-linux/depends/exception.d
+++
@@ -1,9 +0,0 @@
-arch/unknown-i386-linux/depends/exception.d arch/unknown-i386-linux/objects/exception.o: exception.cc ../threads/copyright.h ../threads/system.h \
- ../threads/copyright.h ../threads/utility.h ../threads/bool.h \
- ../machine/sysdep.h ../threads/thread.h ../machine/machine.h \
- ../threads/utility.h ../machine/translate.h ../machine/disk.h \
- ../lab7-8/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
- ../userprog/bitmap.h ../filesys/openfile.h ../threads/list.h \
- ../threads/scheduler.h ../machine/interrupt.h ../threads/list.h \
- ../machine/stats.h ../machine/timer.h ../filesys/synchdisk.h \
- ../machine/disk.h ../threads/synch.h syscall.h
--- lab7-8/arch/unknown-i386-linux/depends/threadtest.d
+++
@@ -1,9 +0,0 @@
-arch/unknown-i386-linux/depends/threadtest.d arch/unknown-i386-linux/objects/threadtest.o: ../threads/threadtest.cc ../threads/copyright.h \
- ../threads/system.h ../threads/utility.h ../threads/bool.h \
- ../machine/sysdep.h ../threads/copyright.h ../threads/thread.h \
- ../machine/machine.h ../threads/utility.h ../machine/translate.h \
- ../machine/disk.h ../lab7-8/addrspace.h ../filesys/filesys.h \
- ../filesys/openfile.h ../userprog/bitmap.h ../filesys/openfile.h \
- ../threads/list.h ../threads/scheduler.h ../machine/interrupt.h \
- ../threads/list.h ../machine/stats.h ../machine/timer.h \
- ../filesys/synchdisk.h ../machine/disk.h ../threads/synch.h
